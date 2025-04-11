a61db486a66f00fb75c710c0aad0ebbde9b17eb5
Haidara <sidymohamedcherifhaidara02@gmail.com>
2025-04-11 15:29:40 +0000

    delete error

--- app/(dashboard_Hotel)/dashboard/hotes/[id]/layout.tsx
+++ app/(dashboard_Hotel)/dashboard/hotes/[id]/layout.tsx
@@ -1,7 +1,13 @@
 import { userIsHotelier } from "@/app/(action)/user.action";
 import ClientLayouts from "@/src/components/ui/Client/ClientLayout";
 
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+    title: "Kims Hotel- Dashboard",
+    description: "Votre Dashboard",
+  };
+  
 
 import { forbidden } from "next/navigation";
 
