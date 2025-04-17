c491802f3ee9a58319b543ade1d563f826e4fcdb
Haidara <sidymohamedcherifhaidara02@gmail.com>
2025-04-17 20:57:14 +0000

    rendu seo

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 import Home from "@/src/components/ui/Client/Home";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Trouver des Hotels et Appartement patout en Guinée",
+  description: "Trouver des Hotel et appartement en un clique",
+};
 
 export default async function Page({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) {
   const resolvedSearchParams = await searchParams; 
