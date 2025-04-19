e9c2249223ef38ac7e1fdc7bbf78be131bba3c30
Haidara <sidymohamedcherifhaidara02@gmail.com>
2025-04-19 23:18:11 +0000

    toujours

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -14,7 +14,6 @@ export async function GET() {
         changefreq: "monthly",
         priority: 0.8,
       },
-    
     ];
   
     const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
