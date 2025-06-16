d367579db6e6acb2cf279a0665c06d2c10ebdb1d
Haidara <sidymohamedcherifhaidara02@gmail.com>
2025-06-16 23:11:21 +0000

    pre rendring

--- src/components/ui/Client/ShowItems.tsx
+++ src/components/ui/Client/ShowItems.tsx
@@ -1,9 +1,11 @@
+// src/components/ui/Client/ShowItems.tsx
 'use client';
 import { useFilter } from "@/contexte/FilterContexte";
 import { homeTypes } from "@/types/types";
 import { useEffect, useState } from "react";
 import ListinCardHome from "../Dashboard/ListinCardHome";
 import { getData } from "@/app/(action)/home.action";
+import { SkeltonCard } from "./skeletonCard";
 
 const getImageUrls = (item: homeTypes) => {
   return item.images?.map((img) => img.urlImage) || [];
@@ -12,16 +14,32 @@ const getImageUrls = (item: homeTypes) => {
 export function ShowItems() {
   const { currentFilter } = useFilter();
   const [items, setItems] = useState<homeTypes[]>([]);
+  const [loading, setLoading] = useState(true);
 
   const selectedOption = currentFilter === "tout" ? undefined : currentFilter;
 
   useEffect(() => {
     async function fetchData() {
-      const data = await getData(selectedOption) as unknown as homeTypes[];
-      setItems(data);
+      try {
+        setLoading(true);
+        const data = await getData(selectedOption) as unknown as homeTypes[];
+        setItems(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
     }
     fetchData();
   }, [selectedOption]);
+
+  if (loading) {
+    return (
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 h-full">
+        {[...Array(6)].map((_, i) => <SkeltonCard key={i} />)}
+      </div>
+    );
+  }
 
   return (
     <>
@@ -41,7 +59,7 @@ export function ShowItems() {
           ))}
         </div>
       ) : (
-        <div className="h-screen flex justify-center items-center text-xl text-gray-500">
+        <div className="h-64 flex justify-center items-center text-xl text-gray-500">
           Aucune donnée trouvée.
         </div>
       )}
